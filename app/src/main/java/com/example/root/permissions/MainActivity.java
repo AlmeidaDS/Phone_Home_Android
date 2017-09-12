f103103fb9c429a3fa8968aedcbc8f628d2df744
Almeida <almeidads85@gmail.com>
2017-09-12 19:07:52 +0100

    Fazendo chamadas com Android

---
+++ app/src/main/java/com/example/root/permissions/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.root.permissions;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    Activity activity = this;
+    private String numeros[] = new String[]{"932987645","987234910","936481694"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = (ListView)findViewById(R.id.lista);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,numeros);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                permissao(numeros[position]);
+            }
+        });
+    }
+    public void iniciarChamada(String numero){
+     Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numero));
+     try {
+         startActivity(intent);
+     }catch (SecurityException e){
+         Toast.makeText(this, "Não temos permissão suficiente para fazer chamadas",Toast.LENGTH_SHORT).show();
+
+     }
+
+    }
+    public void permissao(String numero){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){
+            iniciarChamada(numero);
+        }else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.CALL_PHONE)){
+                Toast.makeText(this, "Precisamos de permissão parar fazer chamadas", Toast.LENGTH_SHORT).show();
+            }else {
+                ActivityCompat.requestPermissions(activity,new String[]{Manifest.permission.CALL_PHONE}, 0);
+            }
+        }
+
+    }
+}
